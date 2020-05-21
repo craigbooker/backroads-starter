0ac3964b8637569a9c08e3fe3393d9cb13a3755b
Craig Booker <817578+craigbooker@users.noreply.github.com>
2020-05-21 14:03:46 -0500

    Adde gatsby-node file to query for the mdx files.

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const { data } = await graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const posts2 = data.allMdx.edges
+
+  posts2.forEach(({ node }) => {
+    const { slug } = node.frontmatter
+    createPage({
+      path: slug,
+      component: require.resolve("./src/templates/post-template.js"),
+      context: {
+        slug: slug,
+      },
+    })
+  })
+}
